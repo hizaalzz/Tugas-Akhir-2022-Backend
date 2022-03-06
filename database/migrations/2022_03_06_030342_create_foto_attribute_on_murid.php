47dbe38f8d038733a8812322bd09a8c9e41b82db
hizaalzz <hizaaliza707@gmail.com>
2022-03-06 10:05:09 +0700

    databasemigrations

---
+++ database/migrations/2022_03_06_030342_create_foto_attribute_on_murid.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foto_attribute_on_murid', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foto_attribute_on_murid');
+    }
+};
