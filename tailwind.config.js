1137958b11a91d78b3bd52c43207d1ebf436a2f9
hizaalzz <hizaaliza707@gmail.com>
2022-03-07 22:00:34 +0700

    resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: [],
+  purge: {
+    enabled: false,
+    content: ['resources/views/**/*.blade.php'],
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {},
+    backgroundColor: ({ after }) => after(['disabled'])
   },
   plugins: [],
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,26 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.js('resources/js/app.js', 'public/js/bundle.js')
+ .sass('resources/sass/app.scss', 'public/css/bundle.css');
+
+mix.js('resources/js/dependencies.js', 'public/js')
+ .styles([
+     'public/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css',
+     'public/css/bootstrap.min.css',
+     'public/css/icons.min.css',
+     'public/css/all.min.css',
+     'public/css/app.min.css',
+ ], 'public/css/template.css');
+
+mix.sass('resources/sass/optional.scss', 'public/css');
+
+mix.sass('resources/sass/datatable.scss', 'public/css');
+
+const tailwindcss = require('tailwindcss');
+
+mix.sass('resources/sass/test.scss', 'public/css')
+.options({
+ processCssUrls: false,
+ postCss: [ tailwindcss('tailwind.config.js') ],
+})
